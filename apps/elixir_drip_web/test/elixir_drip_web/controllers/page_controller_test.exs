eb1f3b04c442082d43f3690ce8e55222ed4a2cad
André Albuquerque <andre.albuquerque@onfido.com>
2018-05-25 22:14:03 +0100

    Travis CI pushing image to gcr.io and change the k8s deployment image
    Also updated the `index.html` template so we can easily see the
    application deployed.

--- apps/elixir_drip_web/test/elixir_drip_web/controllers/page_controller_test.exs
+++ apps/elixir_drip_web/test/elixir_drip_web/controllers/page_controller_test.exs
@@ -3,6 +3,6 @@ defmodule ElixirDripWeb.PageControllerTest do
 
   test "GET /", %{conn: conn} do
     conn = get(conn, "/")
-    assert html_response(conn, 200) =~ "Welcome to ElixirDrip v0.0.8!"
+    assert html_response(conn, 200) =~ "Welcome to ElixirDrip"
   end
 end
--- deploy/travis_deploy.sh
+++ deploy/travis_deploy.sh
@@ -4,10 +4,7 @@ COMMIT="$1"
 ENVIRONMENT="$2"
 echo "Deploying ${COMMIT} to ${ENVIRONMENT}"
 
-# TODO: remove
-docker images
-
-echo $GOOGLE_CLOUD_DEPLOY_CREDENTIALS | base64 --decode > ${HOME}/travis-ci-k8s-deployment-creds.json
+echo ${GOOGLE_CLOUD_DEPLOY_CREDENTIALS} | base64 --decode > ${HOME}/travis-ci-k8s-deployment-creds.json
 
 gcloud auth activate-service-account --key-file ${HOME}/travis-ci-k8s-deployment-creds.json
 
@@ -15,12 +12,17 @@ ZONE=europe-west1-b
 PROJECT_ID=intense-talent-188323
 CLUSTER=elixir-drip-prod-cluster
 
-# TODO: add --quiet after gcloud
-gcloud config set compute/zone $ZONE
-gcloud config set project $PROJECT_ID
-gcloud config set container/cluster $CLUSTER
-gcloud container clusters get-credentials $CLUSTER
+gcloud --quiet config set compute/zone ${ZONE}
+gcloud --quiet config set project ${PROJECT_ID}
+gcloud --quiet config set container/cluster ${CLUSTER}
+gcloud --quiet container clusters get-credentials ${CLUSTER}
 
-gcloud docker -- push gcr.io/${PROJECT_ID}/elixir-drip-prod:${COMMIT}
+yes | gcloud auth configure-docker
+
+docker push gcr.io/${PROJECT_ID}/elixir-drip-prod:${COMMIT}
+
+kubectl -n ${ENVIRONMENT} get pods
+
+kubectl -n ${ENVIRONMENT} set image deployment/elixir-drip elixir-drip-prod=gcr.io/intense-talent-188323/elixir-drip-prod:${COMMIT}
 
 echo "Success!"
