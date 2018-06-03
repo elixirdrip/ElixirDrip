f299c1c48407aeb78c1b47228510cd170bb6952b
Daniel Caixinha <2683004+dcaixinha@users.noreply.github.com>
2018-06-03 23:23:33 +0100

    Add `login_user_with_pw` function to the Accounts context
    This function allows us to provide a username + password pair and check
    if the credentials are correct. If they are, this function returns
    `{:ok, user}` (with `user` being a %User{} struct). If they aren't, this
    function returns `:error`. It's important to note that we're using the
    `dummy_checkpw()` function from the `comeonin` library to simulate the
    check of a password when the user isn't found, to protect our
    application from timing attacks.

--- apps/elixir_drip/lib/elixir_drip/accounts/accounts.ex
+++ apps/elixir_drip/lib/elixir_drip/accounts/accounts.ex
@@ -20,6 +20,17 @@ defmodule ElixirDrip.Accounts do
     |> Repo.get(user_id)
   end
 
+  def login_user_with_pw(username, password) do
+    with %User{} = user <- get_user_by_username(username),
+         true <- verify_user_password(user, password) do
+      {:ok, user}
+    else
+      _ ->
+        Comeonin.Bcrypt.dummy_checkpw()
+        :error
+    end
+  end
+
   def get_user_by_username(username) do
     User
     |> Repo.get_by(username: username)
