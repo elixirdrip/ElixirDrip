0a3b549b0453625893d24fca9e8c6f33f8dae928
Daniel Caixinha <2683004+dcaixinha@users.noreply.github.com>
2018-06-17 18:25:25 +0100

    Add tests within a describe block with invalid file name for changeset

--- apps/elixir_drip/test/elixir_drip/storage/media_test.exs
+++ apps/elixir_drip/test/elixir_drip/storage/media_test.exs
@@ -18,6 +18,22 @@ defmodule ElixirDrip.Storage.MediaTest do
     assert changeset.valid? == true
   end
 
+  describe "when the file name is invalid" do
+    setup :invalid_file_name
+
+    test "the changeset isn't valid", context do
+      changeset = @subject.create_initial_changeset(context.user_id, context.file_name, context.full_path, context.file_size)
+
+      assert changeset.valid? == false
+    end
+
+    test "the changeset contains the expected error message", context do
+      changeset = @subject.create_initial_changeset(context.user_id, context.file_name, context.full_path, context.file_size)
+
+      assert changeset.errors == [file_name: {"Invalid file name", []}]
+    end
+  end
+
   test "is_valid_path?/1 returns {:ok, :valid} for a valid path" do
     path = "$/abc"
 
@@ -28,5 +44,9 @@ defmodule ElixirDrip.Storage.MediaTest do
     path = "$/abc/"
 
     assert {:error, :invalid_path} == @subject.is_valid_path?(path)
+  end
+
+  defp invalid_file_name(_context) do
+    {:ok, file_name: "a/bc" }
   end
 end
