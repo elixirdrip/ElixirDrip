54694c7ad0f4bbe59341f855d141400eb462bcb2
Daniel Caixinha <2683004+dcaixinha@users.noreply.github.com>
2018-06-17 18:24:42 +0100

    Add first test to `Media.create_initial_changeset/4` function

--- apps/elixir_drip/test/elixir_drip/storage/media_test.exs
+++ apps/elixir_drip/test/elixir_drip/storage/media_test.exs
@@ -3,6 +3,21 @@ defmodule ElixirDrip.Storage.MediaTest do
 
   @subject ElixirDrip.Storage.Media
 
+  setup do
+    user_id = "0ujsswThIGTUYm2K8FjOOfXtY1K"
+    file_name = "test.txt"
+    full_path = "$/abc"
+    file_size = 123
+
+    {:ok, user_id: user_id, file_name: file_name, full_path: full_path, file_size: file_size}
+  end
+
+  test "create_initial_changeset/4 returns a valid changeset", context do
+    changeset = @subject.create_initial_changeset(context.user_id, context.file_name, context.full_path, context.file_size)
+
+    assert changeset.valid? == true
+  end
+
   test "is_valid_path?/1 returns {:ok, :valid} for a valid path" do
     path = "$/abc"
 
